c6c1cbd2f716d9cc017d1280155be4e63fa74d3d
jaekim3220 <jehyunkim9@gmail.com>
2024-10-31 20:11:37 +0900

    레이아웃 잡기 (1. 템플릿으로 Layout 지정하기) 진행 수행 - Layout, thymeleaf 문법을 사용해 HTML 구조를 분리

---
+++ src/main/java/com/memo/user/UserController.java
@@ -0,0 +1,29 @@
+package com.memo.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+@Controller + return String => ViewResolver => HTML 파일 렌더링(Model) => HTML
+@Controller + @ResponseBody return String => HTTPMessageConverter => HTML
+@Controller + @ResponseBody return 객체(map, list) => HTTPMessageConverter => jackson => JSON
+@RestController return map => JSON JSON (API 일 경우)
+*/
+
+@Controller // view 영역
+@RequestMapping("/user")
+public class UserController {
+
+	
+	@GetMapping("/sign-up-view")
+	// http:localhost/user/sign-up-view
+	public String signUpView() {
+		// 가운데 Layout(section) 조각만 내려주면
+		// 전체 Layout과 함께 구성된다
+		return "user/signUp";
+	}
+	
+	
+	
+}
