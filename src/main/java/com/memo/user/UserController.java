da7475f3dc33746255fdb8ab4a8ada456dccde50
jaekim3220 <jehyunkim9@gmail.com>
2024-11-04 19:23:37 +0900

    session 정보를 받아와 로그인 화면 구현(로그인 됐을 경우, 안 됐을 경우의 HTML 화면 조정) + SESSION 초기화를 사용한 로그아웃)

--- src/main/java/com/memo/user/UserController.java
+++ src/main/java/com/memo/user/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 /*
 @Controller + return String => ViewResolver => HTML 파일 렌더링(Model) => HTML
 @Controller + @ResponseBody return String => HTTPMessageConverter => HTML
@@ -15,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
-	
+	/**
+	 * 
+	 * @return
+	 */
 	@GetMapping("/sign-up-view")
 	// http:localhost/user/sign-up-view
 	public String signUpView() {
@@ -25,6 +30,10 @@ public class UserController {
 	}
 	
 	
+	/**
+	 * 
+	 * @return
+	 */
 	@GetMapping("/sign-in-view")
 	// http:localhost/user/sign-in-view
 	public String signInView() {
@@ -33,6 +42,17 @@ public class UserController {
 		return "user/signIn";
 	}
 	
+	
+	@GetMapping("/sign-out")
+	public String signOut(HttpSession session) {
+		// session 내용 초기화
+		session.removeAttribute("userId");
+		session.removeAttribute("userLoginId");
+		session.removeAttribute("userName");
+		
+		// 로그인 페이지로 이동(redirect)
+		return "redirect:/user/sign-in-view";
+	}
 	
 	
 }
