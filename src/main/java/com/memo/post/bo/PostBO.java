aebde3a8389e1ac71e5136aca6dd6b3b7e41d8dc
jaekim3220 <jehyunkim9@gmail.com>
2024-11-05 20:31:04 +0900

    구분을 위한 설명 추가

--- src/main/java/com/memo/post/bo/PostBO.java
+++ src/main/java/com/memo/post/bo/PostBO.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.memo.domain.Post;
 import com.memo.mapper.PostMapper;
@@ -17,6 +18,7 @@ public class PostBO {
 	
 	// input : int(userId)
 	// output : List<Post>
+	// @GetMapping("/post-list-view") 구현
 	public List<Post> getPostListByUserId(int userId) {
 		return postMapper.selectPostListByUserId(userId);
 	}
